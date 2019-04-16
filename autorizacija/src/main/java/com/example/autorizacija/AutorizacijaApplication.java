c4c691b783cbd4681c99e68112d4e6264f57005e
spetsnazr <smujic2@etf.unsa.ba>
2019-04-16 12:44:50 +0200

    Dodana privilegija za kreiranje nove zadaće

---
+++ autorizacija/src/main/java/com/example/autorizacija/AutorizacijaApplication.java
@@ -0,0 +1,13 @@
+package com.example.autorizacija;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutorizacijaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutorizacijaApplication.class, args);
+    }
+
+}

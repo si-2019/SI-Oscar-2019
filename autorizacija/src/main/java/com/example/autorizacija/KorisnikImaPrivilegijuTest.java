1334792858927e17268a1aa08b2daaf5bdfe84be
spetsnazr <smujic2@etf.unsa.ba>
2019-05-05 00:28:10 +0200

    Dodan test

---
+++ autorizacija/src/main/java/com/example/autorizacija/KorisnikImaPrivilegijuTest.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Date;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+public class PrivilegijaTest {
+    @Autowired
+    private UlogaRepozitorij ulogaRepozitorij;
+    @Autowired
+    private KorisnikRepozitorij korisnikRepozitorij;
+    @Autowired
+    private OdsjekRepozitorij odsjekRepozitorij;
+
+    @Test
+	public void testProfesorImaMogucnostZaObavjestenje() {
+
+        Long id=(long) (korisnikRepozitorij.count()+1);
+        Korisnik korisnik = new Korisnik(id,odsjekRepozitorij.findBynazivOdsjeka("RI"),ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR), "Nate", "Kll", new Date(1996,5, 16), "0506997178963", "Nate@unsa.ba", "Sarajevo", "KS", "BiH", "1234567", true, "Kll", "Kll", "ISAKA SAMOKOVLIJE", "user", "pass", "Nate@linkedin.com", "Nate@website.com", null, "5281", "1", "6", "ASISTENT");
+        korisnikRepozitorij.save(korisnik);
+        assertEquals(true, (korisnik.imaPrivilegiju("registrovanje-nove-zadace")));
+    }
+
+
+
+}

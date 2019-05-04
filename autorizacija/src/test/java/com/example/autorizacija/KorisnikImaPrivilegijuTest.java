8056ebbe6d37525c19085d5964df2db4d0d758af
Zerina <zerina.ramic@live.com>
2019-05-04 13:28:46 +0200

    izmjene u KorisnikImaPrivilegijuTest

--- autorizacija/src/test/java/com/example/autorizacija/KorisnikImaPrivilegijuTest.java
+++ autorizacija/src/test/java/com/example/autorizacija/KorisnikImaPrivilegijuTest.java
@@ -26,13 +26,14 @@ public class KorisnikImaPrivilegijuTest {
     @Autowired
     private UlogaRepozitorij ulogaRepozitorij;
 
+
     @Test
-    public void testAdminImaPrivilegijuBrisanjaKorisnika() throws Exception {
+    public void testProfesorImaPrivilegijuEditovanjaKreiraneZadace() throws Exception {
         Odsjek odsjek = odsjekRepozitorij.findById(Long.valueOf(1)).get();
-        Uloga uloga = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ADMIN);
-        Korisnik korisnik = new Korisnik(Long.valueOf(2), odsjek, uloga, "Edina", "Kovac", new Date(1997, 2, 4), "0506997178963", "ekovac2@etf.unsa.ba", "Sarajevo", "Sarajevo", "BiH", "062589632", true, "Nakma Kovac", "Fudo Kovac", "Zupca", "ekovac2", "789456", "ekovac@linkedin.com", "ekovac@website.com", null, "17933", "1", "6", "admin");
+        Uloga uloga = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
+        Korisnik korisnik = new Korisnik(Long.valueOf(4), odsjek, uloga, "Ermin", "Sabotic", new Date(1997, 2, 4), "0506997178963", "ekovac2@etf.unsa.ba", "Sarajevo", "Sarajevo", "BiH", "062589632", true, "Nakma Kovac", "Fudo Kovac", "Zupca", "ekovac2", "789456", "ekovac@linkedin.com", "ekovac@website.com", null, "17933", "1", "6", "admin");
         korisnikRepozitorij.save(korisnik);
-        assertThat(korisnik.imaPrivilegiju("brisanje-korisnika")).isEqualTo(true);
+        assertThat(korisnik.imaPrivilegiju("editovanje-kreirane-zadace")).isEqualTo(true);
 
     }
 
@@ -47,12 +48,12 @@ public class KorisnikImaPrivilegijuTest {
     }
 
     @Test
-    public void testProfesorImaPrivilegijuEditovanjaKreiraneZadace() throws Exception {
+    public void testAdminImaPrivilegijuBrisanjaKorisnika() throws Exception {
         Odsjek odsjek = odsjekRepozitorij.findById(Long.valueOf(1)).get();
-        Uloga uloga = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
-        Korisnik korisnik = new Korisnik(Long.valueOf(4), odsjek, uloga, "Ermin", "Sabotic", new Date(1997, 2, 4), "0506997178963", "ekovac2@etf.unsa.ba", "Sarajevo", "Sarajevo", "BiH", "062589632", true, "Nakma Kovac", "Fudo Kovac", "Zupca", "ekovac2", "789456", "ekovac@linkedin.com", "ekovac@website.com", null, "17933", "1", "6", "admin");
+        Uloga uloga = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ADMIN);
+        Korisnik korisnik = new Korisnik(Long.valueOf(2), odsjek, uloga, "Edina", "Kovac", new Date(1997, 2, 4), "0506997178963", "ekovac2@etf.unsa.ba", "Sarajevo", "Sarajevo", "BiH", "062589632", true, "Nakma Kovac", "Fudo Kovac", "Zupca", "ekovac2", "789456", "ekovac@linkedin.com", "ekovac@website.com", null, "17933", "1", "6", "admin");
         korisnikRepozitorij.save(korisnik);
-        assertThat(korisnik.imaPrivilegiju("editovanje-kreirane-zadace")).isEqualTo(true);
+        assertThat(korisnik.imaPrivilegiju("brisanje-korisnika")).isEqualTo(true);
 
     }
 

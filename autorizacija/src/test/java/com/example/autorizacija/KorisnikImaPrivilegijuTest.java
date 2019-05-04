82309da4ed2648674386435bcb3565cedaad0ad5
spetsnazr <smujic2@etf.unsa.ba>
2019-05-05 00:53:52 +0200

    Prepravka

--- autorizacija/src/test/java/com/example/autorizacija/KorisnikImaPrivilegijuTest.java
+++ autorizacija/src/test/java/com/example/autorizacija/KorisnikImaPrivilegijuTest.java
@@ -25,7 +25,7 @@ public class PrivilegijaTest {
 	public void testProfesorImaMogucnostZaObavjestenje() {
 
         Long id=(long) (korisnikRepozitorij.count()+1);
-        Korisnik korisnik = new Korisnik(id,odsjekRepozitorij.findBynazivOdsjeka("RI"),ulogaRepozitorij.findBynazivUloge(ImenaUloga.ADMIN), "Test", "N", new Date(1996,5, 16), "0506997178963", "Testt@unsa.ba", "Sarajevo", "KS", "BiH", "1234567", true, "Kll", "Kll", "ISAKA SAMOKOVLIJE", "user", "pass", "Testt@linkedin.com", "Testt@website.com", null, "5281", "1", "6", "Asdministrator");
+        Korisnik korisnik = new Korisnik(id,odsjekRepozitorij.findBynazivOdsjeka("RI"),ulogaRepozitorij.findBynazivUloge(ImenaUloga.ADMIN), "Test", "N", new Date(1996,5, 16), "0202997178963", "Test6t@unsa.ba", "Sarajevo", "KS", "BiH", "1234567", true, "Kll", "Kll", "ISAKA SAMOKOVLIJE", "user", "pass", "Testt@linkedin.com", "Test6t@website.com", null, "5281", "1", "6", "Administrator");
         korisnikRepozitorij.save(korisnik);
         assertEquals(true, (korisnik.imaPrivilegiju("kreiranje-privilegija")));
     }

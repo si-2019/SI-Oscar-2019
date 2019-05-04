10fc119703a506938a2ff0e4feefdfbf2c62a643
Mahira Buturovic <mbuturovic1@etf.unsa.ba>
2019-05-04 21:09:12 +0200

    izmjene

--- autorizacija_/src/test/java/com/example/demo/PrivilegijaTest.java
+++ autorizacija_/src/test/java/com/example/demo/PrivilegijaTest.java
@@ -22,12 +22,12 @@ public class PrivilegijaTest {
     private OdsjekRepozitorij odsjekRepozitorij;
 
     @Test
-	public void testProfesorImaMogucnostKreiranjaObavjestenja() {
+	public void testProfesorImaMogucnostZaObavjestenje() {
 
         Long id=(long) (korisnikRepozitorij.count()+1);
-        Korisnik korisnik = new Korisnik(id,odsjekRepozitorij.findBynazivOdsjeka("ri"),ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR), "Medina", "Dacic", new Date(1996,5, 16), "0506997178963", "medina@unsa.ba", "Sarajevo", "KS", "BiH", "1234567", true, "Dacic", "Dacic", "ISAKA SAMOKOVLIJE", "user", "pass", "medina@linkedin.com", "medina@website.com", null, "5281", "1", "6", "PROFESOR");
+        Korisnik korisnik = new Korisnik(id,odsjekRepozitorij.findBynazivOdsjeka("RI"),ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR), "Medina", "Dacic", new Date(1996,5, 16), "0506997178963", "medina@unsa.ba", "Sarajevo", "KS", "BiH", "1234567", true, "Dacic", "Dacic", "ISAKA SAMOKOVLIJE", "user", "pass", "medina@linkedin.com", "medina@website.com", null, "5281", "1", "6", "PROFESOR");
         korisnikRepozitorij.save(korisnik);
-        assertEquals(true, (korisnik.imaPrivilegiju("kreiranje_obavjestenja")));
+        assertEquals(true, (korisnik.imaPrivilegiju("mogucnost_za_obavjestenje")));
     }
 
 

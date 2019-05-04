9afe61cebbd1c51941f62ee5b2aff3e9dcfd6b20
Mahira Buturovic <mbuturovic1@etf.unsa.ba>
2019-05-04 20:40:17 +0200

    refaktoring

--- autorizacija_/src/test/java/com/example/demo/PrivilegijaTest.java
+++ autorizacija_/src/test/java/com/example/demo/PrivilegijaTest.java
@@ -25,7 +25,7 @@ public class PrivilegijaTest {
 	public void testAsistentImaMogucnostKreiranjaObavjestenja() {
 
         Long id=(long) (korisnikRepozitorij.count()+1);
-        Korisnik korisnik = new Korisnik(id,odsjekRepozitorij.findBynazivOdsjeka("ri"),ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT), "Amila", "Borancic", new Date(1997,1 , 17), "0506997178963", "amila@unsa.ba", "Sarajevo", "KS", "BiH", "1234567", true, "Dzenana Borancic", "Borancic", "ISAKA SAMOKOVLIJE", "user", "pass", "amila@linkedin.com", "amila@website.com", null, "5281", "1", "6", "ASISTENT");
+        Korisnik korisnik = new Korisnik(id,odsjekRepozitorij.findBynazivOdsjeka("RI"),ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT), "Amila", "Borancic", new Date(1997,1 , 17), "0506997178963", "amila@unsa.ba", "Sarajevo", "KS", "BiH", "1234567", true, "Dzenana Borancic", "Borancic", "ISAKA SAMOKOVLIJE", "user", "pass", "amila@linkedin.com", "amila@website.com", null, "5281", "1", "6", "ASISTENT");
         korisnikRepozitorij.save(korisnik);
         assertEquals(true, (korisnik.imaPrivilegiju("kreiranje_obavjestenja")));
     }

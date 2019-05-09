9c7d4591022427b9f45d0a31abd7d7027b334aa8
Zerina <zerina.ramic@live.com>
2019-05-09 22:00:43 +0200

    dodana nova funkcija u OdsjekRepozitorij

--- autorizacija/src/main/java/com/example/autorizacija/OdsjekRepozitorij.java
+++ autorizacija/src/main/java/com/example/autorizacija/OdsjekRepozitorij.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OdsjekRepozitorij extends JpaRepository<Odsjek, Long> {
     boolean existsBynazivOdsjeka(String naziv);
+    Odsjek findBynazivOdsjeka (String naziv);
 }

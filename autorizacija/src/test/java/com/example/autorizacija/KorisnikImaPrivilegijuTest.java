41fb2b29656d59083a2775bf2e6f3bd11802459a
Zerina <zerina.ramic@live.com>
2019-05-09 22:31:21 +0200

    izmjene u testovima

--- autorizacija/src/test/java/com/example/autorizacija/KorisnikImaPrivilegijuTest.java
+++ autorizacija/src/test/java/com/example/autorizacija/KorisnikImaPrivilegijuTest.java
@@ -29,51 +29,58 @@ public class KorisnikImaPrivilegijuTest {
 
     @Test
     public void testProfesorImaPrivilegijuEditovanjaKreiraneZadace() throws Exception {
-        Odsjek odsjek = odsjekRepozitorij.findById(Long.valueOf(1)).get();
         Uloga uloga = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
-        Korisnik korisnik = new Korisnik(Long.valueOf(4), odsjek, uloga, "Ermin", "Sabotic", new Date(1997, 2, 4), "0506997178963", "ekovac2@etf.unsa.ba", "Sarajevo", "Sarajevo", "BiH", "062589632", true, "Nakma Kovac", "Fudo Kovac", "Zupca", "ekovac2", "789456", "ekovac@linkedin.com", "ekovac@website.com", null, "17933", "1", "6", "admin");
-        korisnikRepozitorij.save(korisnik);
-        assertThat(korisnik.imaPrivilegiju("editovanje-kreirane-zadace")).isEqualTo(true);
+        assertThat(uloga.imaPrivilegiju("editovanje-kreirane-zadace")).isEqualTo(true);
+    }
 
+    @Test
+    public void testProfesorNemaPrivilegijuBrisanjaKorisnika() throws Exception {
+        Uloga uloga = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
+        assertThat(uloga.imaPrivilegiju("brisanje-korisnika")).isEqualTo(false);
     }
 
     @Test
     public void testAsistentImaPrivilegijuBrisanjaKreiraneZadace() throws Exception {
-        Odsjek odsjek = odsjekRepozitorij.findById(Long.valueOf(1)).get();
         Uloga uloga = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT);
-        Korisnik korisnik = new Korisnik(Long.valueOf(3), odsjek, uloga, "Damir", "Pozderac", new Date(1997, 2, 4), "0506997178963", "ekovac2@etf.unsa.ba", "Sarajevo", "Sarajevo", "BiH", "062589632", true, "Nakma Kovac", "Fudo Kovac", "Zupca", "ekovac2", "789456", "ekovac@linkedin.com", "ekovac@website.com", null, "17933", "1", "6", "admin");
-        korisnikRepozitorij.save(korisnik);
-        assertThat(korisnik.imaPrivilegiju("brisanje-kreirane-zadace")).isEqualTo(true);
+        assertThat(uloga.imaPrivilegiju("brisanje-kreirane-zadace")).isEqualTo(true);
+
+    }
+
+    @Test
+    public void testAsistentNemaPrivilegijuBrisanjaKorisnika() throws Exception {
+        Uloga uloga = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT);
+        assertThat(uloga.imaPrivilegiju("brisanje-korisnika")).isEqualTo(false);
 
     }
 
     @Test
     public void testAdminImaPrivilegijuBrisanjaKorisnika() throws Exception {
-        Odsjek odsjek = odsjekRepozitorij.findById(Long.valueOf(1)).get();
         Uloga uloga = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ADMIN);
-        Korisnik korisnik = new Korisnik(Long.valueOf(2), odsjek, uloga, "Edina", "Kovac", new Date(1997, 2, 4), "0506997178963", "ekovac2@etf.unsa.ba", "Sarajevo", "Sarajevo", "BiH", "062589632", true, "Nakma Kovac", "Fudo Kovac", "Zupca", "ekovac2", "789456", "ekovac@linkedin.com", "ekovac@website.com", null, "17933", "1", "6", "admin");
-        korisnikRepozitorij.save(korisnik);
-        assertThat(korisnik.imaPrivilegiju("brisanje-korisnika")).isEqualTo(true);
+        assertThat(uloga.imaPrivilegiju("brisanje-korisnika")).isEqualTo(true);
+    }
 
+    @Test
+    public void testAdminNemaPrivilegijuBrisanjaKreiraneZadace() throws Exception {
+        Uloga uloga = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ADMIN);
+        assertThat(uloga.imaPrivilegiju("brisanje-kreirane-zadace")).isEqualTo(false);
     }
 
     @Test
     public void testAsistentImaPrivilegijuPregledaKreiraneZadace() throws Exception {
-        Odsjek odsjek = odsjekRepozitorij.findById(Long.valueOf(1)).get();
         Uloga uloga = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT);
-        Korisnik korisnik = new Korisnik(Long.valueOf(4), odsjek, uloga, "Damir", "Pozderac", new Date(1997, 2, 4), "0506997178963", "ekovac2@etf.unsa.ba", "Sarajevo", "Sarajevo", "BiH", "062589632", true, "Nakma Kovac", "Fudo Kovac", "Zupca", "ekovac2", "789456", "ekovac@linkedin.com", "ekovac@website.com", null, "17933", "1", "6", "admin");
-        korisnikRepozitorij.save(korisnik);
-        assertThat(korisnik.imaPrivilegiju("pregled-zadace")).isEqualTo(true);
+        assertThat(uloga.imaPrivilegiju("pregled-zadace")).isEqualTo(true);
+    }
 
+    @Test
+    public void testAsistentNemaPrivilegijuKreiranjaKorisnika() throws Exception {
+        Uloga uloga = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT);
+        assertThat(uloga.imaPrivilegiju("kreiranje-korisnika")).isEqualTo(false);
     }
 
     @Test
     public void testProfesorImaPrivilegijuEditovanjaObavjestenja() throws Exception {
-        Odsjek odsjek = odsjekRepozitorij.findById(Long.valueOf(1)).get();
         Uloga uloga = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
-        Korisnik korisnik = new Korisnik(Long.valueOf(4), odsjek, uloga, "Amir", "Pozderac", new Date(1997, 2, 4), "0506997178963", "ekovac2@etf.unsa.ba", "Sarajevo", "Sarajevo", "BiH", "062589632", true, "Nakma Kovac", "Fudo Kovac", "Zupca", "ekovac2", "789456", "ekovac@linkedin.com", "ekovac@website.com", null, "17933", "1", "6", "admin");
-        korisnikRepozitorij.save(korisnik);
-        assertThat(korisnik.imaPrivilegiju("editovanje-obavjestenja")).isEqualTo(true);
+        assertThat(uloga.imaPrivilegiju("editovanje-obavjestenja")).isEqualTo(true);
 
     }
 }

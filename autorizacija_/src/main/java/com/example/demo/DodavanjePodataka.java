b75e7dabcf9afedc7e0c7dbff31f18773a235cf6
mahirabuturovic <mbuturovic1@etf.unsa.ba>
2019-05-10 15:56:14 +0200

    dodana nova uloga za postojecu privilegiju

---
+++ autorizacija_/src/main/java/com/example/demo/DodavanjePodataka.java
@@ -0,0 +1,119 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class DodavanjePodataka {
+
+
+    private KorisnikRepozitorij korisnikRepozitorij;
+    private UlogaRepozitorij ulogaRepozitorij;
+    private PrivilegijaRepozitorij privilegijaRepozitorij;
+    private OdsjekRepozitorij odsjekRepozitorij;
+
+    @Autowired
+    public DodavanjePodataka(KorisnikRepozitorij korisnikRepozitorij, UlogaRepozitorij ulogaRepozitorij,
+            PrivilegijaRepozitorij privilegijaRepozitorij,OdsjekRepozitorij odsjekRepozitorij) {
+        this.korisnikRepozitorij = korisnikRepozitorij;
+        this.ulogaRepozitorij = ulogaRepozitorij;
+        this.privilegijaRepozitorij = privilegijaRepozitorij;
+        this.odsjekRepozitorij=odsjekRepozitorij;
+    }
+
+    @EventListener
+    public void dodaj(ApplicationReadyEvent event) {
+        dodajUloge();
+        dodajPrivilegije();
+        dodajOdsjek();
+    }
+    private void dodajUloge() {
+
+        Uloga uloga1 = new Uloga();
+        uloga1.setNazivUloge(ImenaUloga.ADMIN);
+        if (ulogaRepozitorij.findBynazivUloge(ImenaUloga.ADMIN)==null) ulogaRepozitorij.save(uloga1);
+
+        Uloga uloga2 = new Uloga();
+        uloga2.setNazivUloge(ImenaUloga.STUDENT);
+        if (ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENT)==null) ulogaRepozitorij.save(uloga2);
+
+        Uloga uloga3 = new Uloga();
+        uloga3.setNazivUloge(ImenaUloga.PROFESOR);
+        if (ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR)==null) ulogaRepozitorij.save(uloga3);
+
+        Uloga uloga4 = new Uloga();
+        uloga4.setNazivUloge(ImenaUloga.ASISTENT);
+        if (ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT)==null) ulogaRepozitorij.save(uloga4);
+
+        Uloga uloga5 = new Uloga();
+        uloga5.setNazivUloge(ImenaUloga.STUDENTSKA_SLUZBA);
+        if (ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENTSKA_SLUZBA)==null) ulogaRepozitorij.save(uloga5);
+    }
+
+    private void dodajPrivilegije() {
+
+        List<Uloga> uloga = new ArrayList<>();
+        Uloga uloga1 = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
+        uloga.add(uloga1);
+
+        Privilegija privilegija1 = new Privilegija();
+        privilegija1.setNazivPrivilegije("brisanje_zadace");
+        privilegija1.setUloge(uloga);
+        if(privilegijaRepozitorij.findBynazivPrivilegije("brisanje_zadace")==null) privilegijaRepozitorij.save(privilegija1);
+        
+        Privilegija privilegija2 = new Privilegija();
+        privilegija2.setNazivPrivilegije("registrovanje_casa");
+        privilegija2.setUloge(uloga);
+        if(privilegijaRepozitorij.findBynazivPrivilegije("registrovanje_casa")==null) privilegijaRepozitorij.save(privilegija2);
+
+        Privilegija privilegija3 = new Privilegija();
+        privilegija3.setNazivPrivilegije("editovanje_termina_ispita");
+        privilegija3.setUloge(uloga);
+        if(privilegijaRepozitorij.findBynazivPrivilegije("editovanje_termina_ispita")==null) privilegijaRepozitorij.save(privilegija3);
+
+        uloga = new ArrayList<>();
+        Uloga uloga2 = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT);
+        uloga.add(uloga2);
+        Privilegija privilegija4 = new Privilegija();
+        privilegija4.setNazivPrivilegije("brisanje_teme");
+        privilegija4.setUloge(uloga);
+        if(privilegijaRepozitorij.findBynazivPrivilegije("brisanje_teme")==null) privilegijaRepozitorij.save(privilegija4);
+
+        Privilegija privilegija5 = new Privilegija();
+        privilegija5.setNazivPrivilegije("kreiranje_obavjestenja");
+        privilegija5.setUloge(uloga);
+        if(privilegijaRepozitorij.findBynazivPrivilegije("kreiranje_obavjestenja")==null) privilegijaRepozitorij.save(privilegija5);
+        
+        Privilegija p=privilegijaRepozitorij.findBynazivPrivilegije("kreiranje_obavjestenja");
+        uloga.add(uloga1);        
+        p.setUloge(uloga);
+        privilegijaRepozitorij.save(p);
+
+        uloga=new ArrayList<>();
+        uloga.add(uloga2);
+        Privilegija privilegija6 = new Privilegija();
+        privilegija6.setNazivPrivilegije("brisanje_obavjestenja");
+        privilegija6.setUloge(uloga);
+        if(privilegijaRepozitorij.findBynazivPrivilegije("brisanje_obavjestenja")==null) privilegijaRepozitorij.save(privilegija6);
+
+        p=privilegijaRepozitorij.findBynazivPrivilegije("brisanje_obavjestenja");
+        uloga.add(uloga1);        
+        p.setUloge(uloga);
+        privilegijaRepozitorij.save(p);
+    }
+       
+
+     private void dodajOdsjek(){ 
+
+        if((odsjekRepozitorij.findBynazivOdsjeka("RI"))==null){ 
+            Odsjek odsjek1=new Odsjek();
+            odsjek1.setId((long)(odsjekRepozitorij.count()+1)); 
+            odsjek1.setNazivOdsjeka("RI"); odsjekRepozitorij.save(odsjek1); 
+        } 
+    }
+}
---
+++ autorizacija_/src/test/java/com/example/demo/PrivilegijaTest.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Date;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+public class PrivilegijaTest {
+    @Autowired
+    private UlogaRepozitorij ulogaRepozitorij;
+    @Autowired
+    private KorisnikRepozitorij korisnikRepozitorij;
+    @Autowired
+    private OdsjekRepozitorij odsjekRepozitorij;
+    @Test
+	public void testAsistentImaMogucnostZaBrisanjObavjestenja() {
+        Long id=(long) (korisnikRepozitorij.count()+1);
+        Korisnik korisnik = new Korisnik(id,odsjekRepozitorij.findBynazivOdsjeka("RI"),ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT), "Medina", "Dacic", new Date(1996,5, 16), "0506997178963", "medina@unsa.ba", "Sarajevo", "KS", "BiH", "1234567", true, "Dacic", "Dacic", "ISAKA SAMOKOVLIJE", "user", "pass", "medina@linkedin.com", "medina@website.com", null, "5281", "1", "6", "ASISTENT");
+        korisnikRepozitorij.save(korisnik);
+        assertEquals(true, (korisnik.imaPrivilegiju("brisanje_obavjestenja")));
+    }
+
+
+}

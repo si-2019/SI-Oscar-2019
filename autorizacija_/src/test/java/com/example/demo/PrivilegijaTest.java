b963486e6b194062b0dcaeb2b42c3fc8207582e4
mahirabuturovic <mbuturovic1@etf.unsa.ba>
2019-05-10 15:59:55 +0200

    dodan test-profesor-brisanje-obavještenja

--- autorizacija_/src/test/java/com/example/demo/PrivilegijaTest.java
+++ autorizacija_/src/test/java/com/example/demo/PrivilegijaTest.java
@@ -27,6 +27,13 @@ public class PrivilegijaTest {
         korisnikRepozitorij.save(korisnik);
         assertEquals(true, (korisnik.imaPrivilegiju("brisanje_obavjestenja")));
     }
+    @Test
+	public void testProfesorImaMogucnostZaBrisanjObavjestenja() {
+        Long id=(long) (korisnikRepozitorij.count()+1);
+        Korisnik korisnik = new Korisnik(id,odsjekRepozitorij.findBynazivOdsjeka("RI"),ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR), "Medina", "Dacic", new Date(1996,5, 16), "0506997178963", "medina@unsa.ba", "Sarajevo", "KS", "BiH", "1234567", true, "Dacic", "Dacic", "ISAKA SAMOKOVLIJE", "user", "pass", "medina@linkedin.com", "medina@website.com", null, "5281", "1", "6", "PROFESOR");
+        korisnikRepozitorij.save(korisnik);
+        assertEquals(true, (korisnik.imaPrivilegiju("brisanje_obavjestenja")));
+    }
 
 
 }

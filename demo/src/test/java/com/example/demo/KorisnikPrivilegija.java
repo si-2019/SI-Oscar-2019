93b6b9d9a5043f7e4551e187855a36c53733c5b7
Amina Fajić <37187803+aminaanaan@users.noreply.github.com>
2019-05-10 20:54:11 +0200

    Update KorisnikPrivilegija.java
    izmijenjeni nazivi privilegija

--- demo/src/test/java/com/example/demo/KorisnikPrivilegija.java
+++ demo/src/test/java/com/example/demo/KorisnikPrivilegija.java
@@ -34,7 +34,7 @@ public class KorisnikPrivilegija {
                 "Nahida Manovic", "Fudo Manovic", "Zupca", "ekovac2", "789456", "nedim@linkedin.com",
                 "nedim@website.com", null, "17933", "1", "6", "asistent");
         korisnikRepozitorij.save(korisnik);
-        assertThat(korisnik.imaPrivilegiju("povezivanje_privilegija_uloga")).isEqualTo(true);
+        assertThat(korisnik.imaPrivilegiju("povezivanje-privilegija-uloga")).isEqualTo(true);
 
     }
 }

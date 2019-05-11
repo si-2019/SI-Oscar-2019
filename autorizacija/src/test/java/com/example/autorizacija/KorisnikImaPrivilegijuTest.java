e73769dc84499f7186bacfb02a30dd5829819b40
spetsnazr <smujic2@etf.unsa.ba>
2019-05-11 12:07:52 +0200

    Prepravke

--- autorizacija/src/test/java/com/example/autorizacija/KorisnikImaPrivilegijuTest.java
+++ autorizacija/src/test/java/com/example/autorizacija/KorisnikImaPrivilegijuTest.java
@@ -1,35 +1,40 @@
-package com.example.demo;
+package com.example.autorizacija;
 
-import static org.junit.Assert.assertEquals;
-
-import java.sql.Date;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Date;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
-public class PrivilegijaTest {
-    @Autowired
-    private UlogaRepozitorij ulogaRepozitorij;
+public class KorisnikImaPrivilegijuTest {
+
     @Autowired
     private KorisnikRepozitorij korisnikRepozitorij;
+
     @Autowired
     private OdsjekRepozitorij odsjekRepozitorij;
 
-    @Test
-	public void testProfesorImaMogucnostZaObavjestenje() {
+    @Autowired
+    private UlogaRepozitorij ulogaRepozitorij;
+
+
+    
+	@Test
+	public void testStudentskasluzbapostavljanjaobavjestenja() {
 
         Long id=(long) (korisnikRepozitorij.count()+1);
         Korisnik korisnik = new Korisnik(id,odsjekRepozitorij.findBynazivOdsjeka("RI"),ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENTSKA_SLUZBA), "Test2", "N2", new Date(1996,5, 16), "0806997178963", "Test2t@unsa.ba", "Sarajevo", "KS", "BiH", "1234567", true, "Kll", "Kll", "ISAKA SAMOKOVLIJE", "user", "pass", "Test2t@linkedin.com", "Test2t@website.com", null, "5281", "1", "6", "Studentska Sluzba");
         korisnikRepozitorij.save(korisnik);
         assertEquals(true, (korisnik.imaPrivilegiju("postavljanje-obavjestenja")));
     }
-
-
 
 }

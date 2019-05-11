8bc2fbd0abf39dfa9f4ae9a131f16a5c6098e3fa
mahirabuturovic <mbuturovic1@etf.unsa.ba>
2019-05-11 16:13:23 +0200

    izmjene u repozitorijima

--- autorizacija_/src/main/java/com/example/demo/PrivilegijaRepozitorij.java
+++ autorizacija_/src/main/java/com/example/demo/PrivilegijaRepozitorij.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PrivilegijaRepozitorij extends JpaRepository<Privilegija, Long> {
     Privilegija findBynazivPrivilegije(String naziv);
+    boolean existsBynazivPrivilegije(String privilegija);
 }

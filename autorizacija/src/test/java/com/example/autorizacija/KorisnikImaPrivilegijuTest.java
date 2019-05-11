b866ca01a820a1b6d6f522f74e5e5509b3daa1b0
spetsnazr <smujic2@etf.unsa.ba>
2019-05-11 12:28:49 +0200

    Prepravke

--- autorizacija/src/test/java/com/example/autorizacija/KorisnikImaPrivilegijuTest.java
+++ autorizacija/src/test/java/com/example/autorizacija/KorisnikImaPrivilegijuTest.java
@@ -1,35 +1,39 @@
-package com.example.demo;
+package com.example.autorizacija;
 
-import static org.junit.Assert.assertEquals;
-
-import java.sql.Date;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Date;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
-public class PrivilegijaTest {
-    @Autowired
-    private UlogaRepozitorij ulogaRepozitorij;
+public class KorisnikImaPrivilegijuTest {
+
     @Autowired
     private KorisnikRepozitorij korisnikRepozitorij;
+
     @Autowired
     private OdsjekRepozitorij odsjekRepozitorij;
 
-    @Test
-	public void testProfesorImaMogucnostZaObavjestenje() {
+    @Autowired
+    private UlogaRepozitorij ulogaRepozitorij;
+
+	@Test
+	public void testAsistentImaPrivilegijuKreiranjaTemeNaForumu() {
 
         Long id=(long) (korisnikRepozitorij.count()+1);
         Korisnik korisnik = new Korisnik(id,odsjekRepozitorij.findBynazivOdsjeka("RI"),ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT), "Test", "N", new Date(1996,5, 16), "1212997178963", "Testt@unsa.ba", "Sarajevo", "KS", "BiH", "1234567", true, "Kll", "Kll", "ISAKA SAMOKOVLIJE", "user", "pass", "Testt@linkedin.com", "Testt@website.com", null, "5281", "1", "6", "ASISTENT");
         korisnikRepozitorij.save(korisnik);
         assertEquals(true, (korisnik.imaPrivilegiju("kreiranje-teme-na-forumu")));
     }
-
-
+    
 
 }

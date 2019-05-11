2f74f4c0d6e2cb9d6b760c732eb36d504a727f54
mahirabuturovic <mbuturovic1@etf.unsa.ba>
2019-05-11 16:26:51 +0200

    izmjene

--- autorizacija_/src/main/java/com/example/demo/DodavanjePodataka.java
+++ autorizacija_/src/main/java/com/example/demo/DodavanjePodataka.java
@@ -100,8 +100,19 @@ public class DodavanjePodataka {
 
         if((odsjekRepozitorij.findBynazivOdsjeka("RI"))==null){ 
             Odsjek odsjek1=new Odsjek();
-            odsjek1.setId((long)(odsjekRepozitorij.count()+1)); 
             odsjek1.setNazivOdsjeka("RI"); odsjekRepozitorij.save(odsjek1); 
+        } 
+        if((odsjekRepozitorij.findBynazivOdsjeka("TK"))==null){ 
+            Odsjek odsjek1=new Odsjek();
+            odsjek1.setNazivOdsjeka("TK"); odsjekRepozitorij.save(odsjek1); 
+        } 
+        if((odsjekRepozitorij.findBynazivOdsjeka("EE"))==null){ 
+            Odsjek odsjek1=new Odsjek();
+            odsjek1.setNazivOdsjeka("EE"); odsjekRepozitorij.save(odsjek1); 
+        } 
+        if((odsjekRepozitorij.findBynazivOdsjeka("AIE"))==null){ 
+            Odsjek odsjek1=new Odsjek();
+            odsjek1.setNazivOdsjeka("AIE"); odsjekRepozitorij.save(odsjek1); 
         } 
     }
 }

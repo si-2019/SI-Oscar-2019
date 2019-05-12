b2c591ac2680997ccd14b1920738336b050f107b
spetsnazr <smujic2@etf.unsa.ba>
2019-05-12 15:11:10 +0200

    Prepravke #2

--- autorizacija/src/test/java/com/example/autorizacija/KorisnikImaPrivilegijuTest.java
+++ autorizacija/src/test/java/com/example/autorizacija/KorisnikImaPrivilegijuTest.java
@@ -27,11 +27,8 @@ public class KorisnikImaPrivilegijuTest {
     private UlogaRepozitorij ulogaRepozitorij;
 	@Test
 	public void testAdmministratorImaUloguKreiranjaPrivilegija() {
-
-        Long id=(long) (korisnikRepozitorij.count()+1);
-        Korisnik korisnik = new Korisnik(id,odsjekRepozitorij.findBynazivOdsjeka("RI"),ulogaRepozitorij.findBynazivUloge(ImenaUloga.ADMIN), "Test", "N", new Date(1996,5, 16), "0202997178963", "Test6t@unsa.ba", "Sarajevo", "KS", "BiH", "1234567", true, "Kll", "Kll", "ISAKA SAMOKOVLIJE", "user", "pass", "Testt@linkedin.com", "Test6t@website.com", null, "5281", "1", "6", "Administrator");
-        korisnikRepozitorij.save(korisnik);
-        assertEquals(true, (korisnik.imaPrivilegiju("kreiranje-privilegija")));
+		Uloga uloga = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ADMIN);
+		assertThat(uloga.imaPrivilegiju("kreiranje-privilegija")).isEqualTo(true);
     }
 
 }

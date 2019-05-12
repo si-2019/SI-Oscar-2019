24d735d4b67866396fc1c5326c40e443d450a0ee
spetsnazr <smujic2@etf.unsa.ba>
2019-05-12 21:49:21 +0200

    Nova privilegija i testovi

---
+++ autorizacija/src/main/java/com/example/autorizacija/DodavanjePodataka.java
@@ -0,0 +1,85 @@
+package com.example.autorizacija;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class DodavanjePodataka {
+
+    private KorisnikRepozitorij korisnikRepozitorij;
+    private UlogaRepozitorij ulogaRepozitorij;
+    private PrivilegijaRepozitorij privilegijaRepozitorij;
+    private OdsjekRepozitorij odsjekRepozitorij;
+
+    @Autowired
+    public DodavanjePodataka(KorisnikRepozitorij korisnikRepozitorij, UlogaRepozitorij ulogaRepozitorij, PrivilegijaRepozitorij privilegijaRepozitorij, OdsjekRepozitorij odsjekRepozitorij) {
+        this.korisnikRepozitorij = korisnikRepozitorij;
+        this.ulogaRepozitorij = ulogaRepozitorij;
+        this.privilegijaRepozitorij = privilegijaRepozitorij;
+        this.odsjekRepozitorij = odsjekRepozitorij;
+    }
+
+    @EventListener
+    public void dodaj (ApplicationReadyEvent event){
+        dodajUloge();
+        dodajPrivilegije();
+        dodajOdsjek();
+    }
+
+
+    private void dodajUloge() {
+
+        Uloga uloga1 = new Uloga();
+        uloga1.setNazivUloge(ImenaUloga.ADMIN);
+        if(!ulogaRepozitorij.existsBynazivUloge(ImenaUloga.ADMIN)) ulogaRepozitorij.save(uloga1);
+
+        Uloga uloga2 = new Uloga();
+        uloga2.setNazivUloge(ImenaUloga.STUDENT);
+        if(!ulogaRepozitorij.existsBynazivUloge(ImenaUloga.STUDENT)) ulogaRepozitorij.save(uloga2);
+
+        Uloga uloga3 = new Uloga();
+        uloga3.setNazivUloge(ImenaUloga.PROFESOR);
+        if(!ulogaRepozitorij.existsBynazivUloge(ImenaUloga.PROFESOR)) ulogaRepozitorij.save(uloga3);
+
+        Uloga uloga4 = new Uloga();
+        uloga4.setNazivUloge(ImenaUloga.ASISTENT);
+        if(!ulogaRepozitorij.existsBynazivUloge(ImenaUloga.ASISTENT)) ulogaRepozitorij.save(uloga4);
+
+        Uloga uloga5 = new Uloga();
+        uloga5.setNazivUloge(ImenaUloga.STUDENTSKA_SLUZBA);
+        if(!ulogaRepozitorij.existsBynazivUloge(ImenaUloga.STUDENTSKA_SLUZBA)) ulogaRepozitorij.save(uloga5);
+
+    }
+
+
+    private void dodajPrivilegije() {
+
+
+        List<Uloga> uloga = new ArrayList<>();
+        Uloga uloga1 = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
+        uloga.add(uloga1);
+
+        Privilegija privilegija2 = new Privilegija();
+        privilegija2.setNazivPrivilegije("izmjena-bodova-za-ispite");
+        privilegija2.setUloge(uloga);
+        if(!privilegijaRepozitorij.existsBynazivPrivilegije("izmjena-bodova-za-ispite")) privilegijaRepozitorij.save(privilegija2);
+		
+    }
+
+
+    private void dodajOdsjek() {
+        Odsjek odsjek1 = new Odsjek();
+        odsjek1.setNazivOdsjeka("RI");
+        if(!odsjekRepozitorij.existsBynazivOdsjeka("RI")) odsjekRepozitorij.save(odsjek1);
+
+        Odsjek odsjek2 = new Odsjek();
+        odsjek2.setNazivOdsjeka("TK");
+        if(!odsjekRepozitorij.existsBynazivOdsjeka("TK")) odsjekRepozitorij.save(odsjek2);
+    }
+
+}

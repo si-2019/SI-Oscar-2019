a00eb290e255c24ec64d9c624bb36500290cc500
mahirabuturovic <mbuturovic1@etf.unsa.ba>
2019-05-12 03:37:23 +0200

    nove verzije fajlova

--- autorizacija_/src/main/java/com/example/demo/DodavanjePodataka.java
+++ autorizacija_/src/main/java/com/example/demo/DodavanjePodataka.java
@@ -1,6 +1,4 @@
-﻿    
 package com.example.demo;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;

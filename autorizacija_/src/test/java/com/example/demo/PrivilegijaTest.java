ad4d03032c960dd39f3acd89d19e3642210c71f6
mahirabuturovic <mbuturovic1@etf.unsa.ba>
2019-05-12 02:55:07 +0200

    nove verzije fajlova

--- autorizacija_/src/test/java/com/example/demo/PrivilegijaTest.java
+++ autorizacija_/src/test/java/com/example/demo/PrivilegijaTest.java
@@ -1,7 +1,5 @@
 package com.example.demo;
-
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;

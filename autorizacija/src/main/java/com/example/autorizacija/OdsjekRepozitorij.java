c1539f2160cdfc68030c00cfae0ad16da847a2ca
spetsnazr <smujic2@etf.unsa.ba>
2019-05-12 15:07:20 +0200

    Prepravke #2

--- autorizacija/src/main/java/com/example/autorizacija/OdsjekRepozitorij.java
+++ autorizacija/src/main/java/com/example/autorizacija/OdsjekRepozitorij.java
@@ -1,7 +1,10 @@
 package com.example.autorizacija;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OdsjekRepozitorij extends JpaRepository<Odsjek, Long> {
     boolean existsBynazivOdsjeka(String naziv);
+    Odsjek findBynazivOdsjeka (String naziv);
 }

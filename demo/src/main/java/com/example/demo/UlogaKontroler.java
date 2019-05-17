bbfc3466dbdfa4e361f46bcb0945a45b3e956e0e
Zerina <zerina.ramic@live.com>
2019-05-17 14:18:51 +0200

    dodan projekat

---
+++ demo/src/main/java/com/example/demo/UlogaKontroler.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+public class UlogaKontroler {
+
+    private UlogaRepozitorij ulogaRepozitorij;
+
+    @Autowired
+    public UlogaKontroler(UlogaRepozitorij ulogaRepozitorij) {
+        this.ulogaRepozitorij = ulogaRepozitorij;
+    }
+
+    @RequestMapping(value = "/{idUloge}/{privilegija}", method = RequestMethod.GET)
+    public boolean ulogaImaPrivilegiju(@PathVariable Long idUloge, @PathVariable String privilegija) {
+        if(ulogaRepozitorij.findById(idUloge).equals(Optional.empty())) {
+            return false;
+        }
+        return ulogaRepozitorij.findById(idUloge).get().imaPrivilegiju(privilegija);
+    }
+}

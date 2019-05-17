8a1bad4d062b3e7ea6f29a17fc95d342148dae56
mahirabuturovic <mbuturovic1@etf.unsa.ba>
2019-05-17 03:21:17 +0200

    dodan api koji vraca sve privilegije

--- autorizacija_/src/main/java/com/example/demo/PrivilegijaKontroler.java
+++ autorizacija_/src/main/java/com/example/demo/PrivilegijaKontroler.java
@@ -3,4 +3,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PrivilegijaKontroler {
+    
+    private PrivilegijaRepozitorij privilegijaRepozitorij;
+
+    @Autowired
+    public PrivilegijaKontroler(PrivilegijaRepozitorij privilegijaRepozitorij) {
+        this.privilegijaRepozitorij = privilegijaRepozitorij;
+    }
+
+    @RequestMapping(value = "/privilegije", method = RequestMethod.GET)
+    public List<String> getAllPrivilegije() {
+        List<Privilegija> privilegije = privilegijaRepozitorij.findAll();
+        List<String> nazivi = new ArrayList<String>();
+        for(Privilegija p : privilegije) {
+            nazivi.add(p.getNazivPrivilegije());
+        }
+        return nazivi;
+    }
 }

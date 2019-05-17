8d0513b1b2413947a8ae7d41511ad28f43850dbf
mahirabuturovic <mbuturovic1@etf.unsa.ba>
2019-05-17 05:05:30 +0200

    api za vracanje privilegija

--- autorizacija_/src/main/java/com/example/demo/PrivilegijaKontroler.java
+++ autorizacija_/src/main/java/com/example/demo/PrivilegijaKontroler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 import java.util.List;
-
+ 
 
 @RestController
 public class PrivilegijaKontroler {

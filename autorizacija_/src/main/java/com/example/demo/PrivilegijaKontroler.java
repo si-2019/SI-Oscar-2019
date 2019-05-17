46e3795be974abf6114daa39d0f4d920f9954f92
mahirabuturovic <mbuturovic1@etf.unsa.ba>
2019-05-17 05:03:05 +0200

    swagger

---
+++ autorizacija_/src/main/java/com/example/demo/PrivilegijaKontroler.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
+import java.util.List;
+
+
+@RestController
+public class PrivilegijaKontroler {
+    
+    private PrivilegijaRepozitorij privilegijaRepozitorij;
+
+    @Autowired
+    public PrivilegijaKontroler(PrivilegijaRepozitorij privilegijaRepozitorij) {
+        this.privilegijaRepozitorij = privilegijaRepozitorij;
+    }
+
+    @RequestMapping(value = "/privilegije", method = RequestMethod.GET)
+    public List<String> getAllPrivilegije() {
+        List<Privilegija> privilegije = privilegijaRepozitorij.findAll();
+        List<String> nazivi = new ArrayList<String>();
+        for(Privilegija p : privilegije) {
+            nazivi.add(p.getNazivPrivilegije());
+        }
+        return nazivi;
+    }
+}

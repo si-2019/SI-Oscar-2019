e1e7c3dc063423473d7761d4842b326922aa6c5a
Amina Fajić <37187803+aminaanaan@users.noreply.github.com>
2019-05-19 18:52:45 +0200

    Update DemoApplication.java
    commit

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Import(SwaggerConfig.class)
 @SpringBootApplication
 public class DemoApplication {
 

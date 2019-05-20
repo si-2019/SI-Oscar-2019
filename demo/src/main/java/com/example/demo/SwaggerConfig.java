c33096919924e473fc2039bfcc9c795d12bf788d
Amina Fajić <37187803+aminaanaan@users.noreply.github.com>
2019-05-20 19:35:16 +0200

    Update SwaggerConfig.java
    commit

--- demo/src/main/java/com/example/demo/SwaggerConfig.java
+++ demo/src/main/java/com/example/demo/SwaggerConfig.java
@@ -5,6 +5,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+package com.example.demo;
 
 @Configuration
 @EnableSwagger2

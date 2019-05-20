8069b10692144d5d6f620cbb52809350472e72ed
Amina Fajić <37187803+aminaanaan@users.noreply.github.com>
2019-05-20 19:30:38 +0200

    Update UlogaRepozitorij.java
    commit

--- demo/src/main/java/com/example/demo/UlogaRepozitorij.java
+++ demo/src/main/java/com/example/demo/UlogaRepozitorij.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UlogaRepozitorij extends JpaRepository<Uloga, Long> {
     Uloga findBynazivUloge (ImenaUloga naziv);
+   boolean existsBynazivUloge(ImenaUloga naziv);
+
 }

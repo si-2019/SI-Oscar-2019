ab4200691af35a2c3710eff96fd133722eef7b42
mahirabuturovic <mbuturovic1@etf.unsa.ba>
2019-05-23 17:47:23 +0200

    prepravka

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
 @SpringBootApplication
 @Import(SwaggerConfig.class)
 public class DemoApplication {

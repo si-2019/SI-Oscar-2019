dddc8399580e263a13ba189be2ebdc8cd3cfc6a4
spetsnazr <smujic2@etf.unsa.ba>
2019-05-24 16:08:42 +0200

    Nova privilegija

---
+++ demo/src/test/java/com/example/demo/KorisnikImaPrivilegijuTest.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+public class KorisnikImaPrivilegijuTest {
+
+    @Autowired
+    private KorisnikRepozitorij korisnikRepozitorij;
+
+    @Autowired
+    private OdsjekRepozitorij odsjekRepozitorij;
+
+    @Autowired
+    private UlogaRepozitorij ulogaRepozitorij;
+
+
+    @Test
+    public void testStudentskaSluzbaImaPrivilegijuEditovanjaObavjestenja() throws Exception {
+        Uloga uloga = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
+        assertThat(uloga.imaPrivilegiju("editovanje-obavjestenja")).isEqualTo(true);
+    }
+
+   
+}

6c4a581314340a42d1aa83da1dee71b6fbcc945d
spetsnazr <smujic2@etf.unsa.ba>
2019-05-24 16:04:45 +0200

    Nove privilegije

---
+++ demo/src/main/java/com/example/demo/Podaci.java
@@ -0,0 +1,165 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class Podaci {
+
+    private UlogaRepozitorij ulogaRepozitorij;
+    private PrivilegijaRepozitorij privilegijaRepozitorij;
+
+    @Autowired
+    public Podaci(UlogaRepozitorij ulogaRepozitorij, PrivilegijaRepozitorij privilegijaRepozitorij) {
+        this.ulogaRepozitorij = ulogaRepozitorij;
+        this.privilegijaRepozitorij = privilegijaRepozitorij;
+    }
+
+    @EventListener
+    public void dodaj (ApplicationReadyEvent event){
+        dodajUloge();
+        dodajPrivilegije();
+    }
+
+    private void dodajUloge() {
+        Uloga admin = new Uloga();
+        admin.setNazivUloge(ImenaUloga.ADMIN);
+        if(!ulogaRepozitorij.existsBynazivUloge(ImenaUloga.ADMIN)) ulogaRepozitorij.save(admin);
+
+        Uloga student = new Uloga();
+        student.setNazivUloge(ImenaUloga.STUDENT);
+        if(!ulogaRepozitorij.existsBynazivUloge(ImenaUloga.STUDENT)) ulogaRepozitorij.save(student);
+
+        Uloga profesor = new Uloga();
+        profesor.setNazivUloge(ImenaUloga.PROFESOR);
+        if(!ulogaRepozitorij.existsBynazivUloge(ImenaUloga.PROFESOR)) ulogaRepozitorij.save(profesor);
+
+        Uloga asistent = new Uloga();
+        asistent.setNazivUloge(ImenaUloga.ASISTENT);
+        if(!ulogaRepozitorij.existsBynazivUloge(ImenaUloga.ASISTENT)) ulogaRepozitorij.save(asistent);
+
+        Uloga studentska_sluzba = new Uloga();
+        studentska_sluzba.setNazivUloge(ImenaUloga.STUDENTSKA_SLUZBA);
+        if(!ulogaRepozitorij.existsBynazivUloge(ImenaUloga.STUDENTSKA_SLUZBA)) ulogaRepozitorij.save(studentska_sluzba);
+    }
+
+    private void poveziUloguPrivilegiju(Privilegija privilegija, Uloga uloga) {
+        if(!privilegijaRepozitorij.existsBynazivPrivilegije(privilegija.getNazivPrivilegije())) {
+            privilegijaRepozitorij.save(privilegija);
+        }
+        else {
+            List<Uloga> noveUloge = privilegijaRepozitorij.findBynazivPrivilegije(privilegija.getNazivPrivilegije()).getUloge();
+            boolean potrebno_dodati = true;
+            for (Uloga uloga1: noveUloge) {
+                if(uloga1.getNazivUloge().equals(uloga.getNazivUloge())) potrebno_dodati = false;
+            }
+            if(potrebno_dodati) {
+                noveUloge.add(uloga);
+                privilegijaRepozitorij.deleteById(privilegijaRepozitorij.findBynazivPrivilegije(privilegija.getNazivPrivilegije()).getId());
+                privilegija.setUloge(noveUloge);
+                privilegijaRepozitorij.save(privilegija);
+            }
+        }
+    }
+
+    private void dodajPrivilegije() {
+        List<Uloga> uloga = new ArrayList<>();
+        Uloga profesor = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
+        Uloga admin = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ADMIN);
+        Uloga asistent = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT);
+        Uloga student = ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENT);
+        Uloga studentska_sluzba = ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENTSKA_SLUZBA);
+
+        uloga.add(admin);
+        // PRIVILEGIJE ZA ADMINA
+
+        Privilegija privilegija_editovanje_korisnika = new Privilegija();
+        privilegija_editovanje_korisnika.setNazivPrivilegije("editovanje-korisnika");
+        privilegija_editovanje_korisnika.setUloge(uloga);
+        poveziUloguPrivilegiju(privilegija_editovanje_korisnika, admin);
+
+        Privilegija privilegija_brisanje_obavjestenja = new Privilegija();
+        privilegija_brisanje_obavjestenja.setNazivPrivilegije("brisanje-obavjestenja");
+        privilegija_brisanje_obavjestenja.setUloge(uloga);
+        poveziUloguPrivilegiju(privilegija_brisanje_obavjestenja, admin);
+        uloga.clear();
+
+        uloga.add(asistent);
+        // PRIVILEGIJE ZA ASISTENTA
+
+        Privilegija privilegija_registrovanje_casa = new Privilegija();
+        privilegija_registrovanje_casa.setNazivPrivilegije("registrovanje-casa");
+        privilegija_registrovanje_casa.setUloge(uloga);
+        poveziUloguPrivilegiju(privilegija_registrovanje_casa, asistent);
+
+        Privilegija privilegija_editovanje_zadace = new Privilegija();
+        privilegija_editovanje_zadace.setNazivPrivilegije("editovanje-kreirane-zadace");
+        privilegija_editovanje_zadace.setUloge(uloga);
+        poveziUloguPrivilegiju(privilegija_editovanje_zadace, asistent);
+
+        Privilegija privilegija_editovanje_obavjestenja = new Privilegija();
+        privilegija_editovanje_obavjestenja.setNazivPrivilegije("editovanje-obavjestenja");
+        privilegija_editovanje_obavjestenja.setUloge(uloga);
+        poveziUloguPrivilegiju(privilegija_editovanje_obavjestenja, asistent);
+
+        Privilegija privilegija_editovanje_teme_forum = new Privilegija();
+        privilegija_editovanje_teme_forum.setNazivPrivilegije("editovanje-teme-na-forumu");
+        privilegija_editovanje_teme_forum.setUloge(uloga);
+        poveziUloguPrivilegiju(privilegija_editovanje_teme_forum, asistent);
+
+        Privilegija privilegija_pristup_grupama = new Privilegija();
+        privilegija_pristup_grupama.setNazivPrivilegije("pristup-grupama-saradnik");
+        privilegija_pristup_grupama.setUloge(uloga);
+        poveziUloguPrivilegiju(privilegija_pristup_grupama, asistent);
+
+        Privilegija privilegija_uvid_u_komentare = new Privilegija();
+        privilegija_uvid_u_komentare.setNazivPrivilegije("uvid-u-komentare-saradnik");
+        privilegija_uvid_u_komentare.setUloge(uloga);
+        poveziUloguPrivilegiju(privilegija_uvid_u_komentare, asistent);
+        uloga.clear();
+
+        uloga.add(profesor);
+        // PRIVILEGIJE ZA PROFESORA
+
+        Privilegija privilegija_izmjena_bodova_zadace = new Privilegija();
+        privilegija_izmjena_bodova_zadace.setNazivPrivilegije("izmjena-bodova-za-zadace");
+        privilegija_izmjena_bodova_zadace.setUloge(uloga);
+        poveziUloguPrivilegiju(privilegija_izmjena_bodova_zadace, profesor);
+
+        Privilegija privilegija_brisanje_kreiranog_casa = new Privilegija();
+        privilegija_brisanje_kreiranog_casa.setNazivPrivilegije("brisanje-kreiranog-casa");
+        privilegija_brisanje_kreiranog_casa.setUloge(uloga);
+        poveziUloguPrivilegiju(privilegija_brisanje_kreiranog_casa, profesor);
+
+        Privilegija privilegija_dodjela_bodova_zadace = new Privilegija();
+        privilegija_dodjela_bodova_zadace.setNazivPrivilegije("dodjela-bodova-za-zadace");
+        privilegija_dodjela_bodova_zadace.setUloge(uloga);
+        poveziUloguPrivilegiju(privilegija_dodjela_bodova_zadace, profesor);
+
+        Privilegija privilegija_kreiranje_kviza = new Privilegija();
+        privilegija_kreiranje_kviza.setNazivPrivilegije("kreiranje-kviza");
+        privilegija_kreiranje_kviza.setUloge(uloga);
+        poveziUloguPrivilegiju(privilegija_kreiranje_kviza, profesor);
+
+        Privilegija privilegija_pregled_predmeta = new Privilegija();
+        privilegija_pregled_predmeta.setNazivPrivilegije("pregled-predmeta-saradnik");
+        privilegija_pregled_predmeta.setUloge(uloga);
+        poveziUloguPrivilegiju(privilegija_pregled_predmeta, profesor);
+
+        Privilegija privilegija_nacin_slanja = new Privilegija();
+        privilegija_nacin_slanja.setNazivPrivilegije("izbor-nacina-slanja-zadace");
+        privilegija_nacin_slanja.setUloge(uloga);
+        poveziUloguPrivilegiju(privilegija_nacin_slanja, profesor);
+
+        Privilegija privilegija_unos_konacne_ocjene = new Privilegija();
+        privilegija_unos_konacne_ocjene.setNazivPrivilegije("unos-konacne-ocjene");
+        privilegija_unos_konacne_ocjene.setUloge(uloga);
+        poveziUloguPrivilegiju(privilegija_unos_konacne_ocjene, profesor);
+        uloga.clear();
+    }
+}
---
+++ demo/src/test/java/com/example/demo/TestoviPrivilegija.java
@@ -0,0 +1,287 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.sql.Date;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+
+public class TestoviPrivilegija {
+    @Autowired
+    private KorisnikRepozitorij korisnikRepozitorij;
+
+    @Autowired
+    private OdsjekRepozitorij odsjekRepozitorij;
+
+    @Autowired
+    private UlogaRepozitorij ulogaRepozitorij;
+
+    @Autowired
+    private PrivilegijaRepozitorij privilegijaRepozitorij;
+
+    @Test
+    public void testPostojiRegistrovanjeCasa() throws Exception {
+        assertThat(privilegijaRepozitorij.existsBynazivPrivilegije("registrovanje-casa")).isEqualTo(true);
+    }
+
+    @Test
+    public void testAsistentRegistrovanjeCasa() throws Exception {
+        Uloga asistent = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT);
+        assertThat(asistent.imaPrivilegiju("registrovanje-casa")).isEqualTo(true);
+    }
+
+    @Test
+    public void testStudentRegistrovanjeCasa() throws Exception {
+        Uloga student = ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENT);
+        assertThat(student.imaPrivilegiju("registrovanje-casa")).isEqualTo(false);
+    }
+
+    @Test
+    public void testPostojiEditovanjeZadace() throws Exception {
+        assertThat(privilegijaRepozitorij.existsBynazivPrivilegije("editovanje-kreirane-zadace")).isEqualTo(true);
+    }
+
+    @Test
+    public void testAsistentEditovanjeZadace() throws Exception {
+        Uloga asistent = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT);
+        assertThat(asistent.imaPrivilegiju("editovanje-kreirane-zadace")).isEqualTo(true);
+    }
+
+    @Test
+    public void testStudentEditovanjeZadace() throws Exception {
+        Uloga student = ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENT);
+        assertThat(student.imaPrivilegiju("editovanje-kreirane-zadace")).isEqualTo(false);
+    }
+
+    @Test
+    public void testPostojiIzmjenaBodovaZadace() throws Exception {
+        assertThat(privilegijaRepozitorij.existsBynazivPrivilegije("izmjena-bodova-za-zadace")).isEqualTo(true);
+    }
+
+    @Test
+    public void testProfesorIzmjenaBodovaZadace() throws Exception {
+        Uloga profesor = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
+        assertThat(profesor.imaPrivilegiju("izmjena-bodova-za-zadace")).isEqualTo(true);
+    }
+
+    @Test
+    public void testStudentIzmjenaBodovaZadace() throws Exception {
+        Uloga student = ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENT);
+        assertThat(student.imaPrivilegiju("izmjena-bodova-za-zadace")).isEqualTo(false);
+    }
+
+    @Test
+    public void testPostojiBrisanjeKreiranogCasa() throws Exception {
+        assertThat(privilegijaRepozitorij.existsBynazivPrivilegije("brisanje-kreiranog-casa")).isEqualTo(true);
+    }
+
+    @Test
+    public void testProfesorBrisanjeKreiranogCasa() throws Exception {
+        Uloga profesor = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
+        assertThat(profesor.imaPrivilegiju("brisanje-kreiranog-casa")).isEqualTo(true);
+    }
+
+    @Test
+    public void testStudentBrisanjeKreiranogCasa() throws Exception {
+        Uloga student = ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENT);
+        assertThat(student.imaPrivilegiju("brisanje-kreiranog-casa")).isEqualTo(false);
+    }
+
+    @Test
+    public void testPostojiDodjelaBodovaZadace() throws Exception {
+        assertThat(privilegijaRepozitorij.existsBynazivPrivilegije("dodjela-bodova-za-zadace")).isEqualTo(true);
+    }
+
+    @Test
+    public void testProfesorDodjelaBodovaZadace() throws Exception {
+        Uloga profesor = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
+        assertThat(profesor.imaPrivilegiju("dodjela-bodova-za-zadace")).isEqualTo(true);
+    }
+
+    @Test
+    public void testStudentskaDodjelaBodovaZadace() throws Exception {
+        Uloga studentska = ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENTSKA_SLUZBA);
+        assertThat(studentska.imaPrivilegiju("dodjela-bodova-za-zadace")).isEqualTo(false);
+    }
+
+    @Test
+    public void testPostojiKreiranjeKviza() throws Exception {
+        assertThat(privilegijaRepozitorij.existsBynazivPrivilegije("kreiranje-kviza")).isEqualTo(true);
+    }
+
+    @Test
+    public void testProfesorKreiranjeKviza() throws Exception {
+        Uloga profesor = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
+        assertThat(profesor.imaPrivilegiju("kreiranje-kviza")).isEqualTo(true);
+    }
+
+    @Test
+    public void testStudentskaKreiranjeKviza() throws Exception {
+        Uloga studentska = ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENTSKA_SLUZBA);
+        assertThat(studentska.imaPrivilegiju("kreiranje-kviza")).isEqualTo(false);
+    }
+
+    @Test
+    public void testEditovanjeObavjestenja() throws Exception {
+        assertThat(privilegijaRepozitorij.existsBynazivPrivilegije("editovanje-obavjestenja")).isEqualTo(true);
+    }
+
+    @Test
+    public void testAsistentEditovanjeObavjestenja() throws Exception {
+        Uloga asistent = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT);
+        assertThat(asistent.imaPrivilegiju("editovanje-obavjestenja")).isEqualTo(true);
+    }
+
+    @Test
+    public void testStudentEditovanjeObavjestenja() throws Exception {
+        Uloga student = ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENT);
+        assertThat(student.imaPrivilegiju("editovanje-obavjestenja")).isEqualTo(false);
+    }
+
+    @Test
+    public void testPostojiEditovanjeTemeForum() throws Exception {
+        assertThat(privilegijaRepozitorij.existsBynazivPrivilegije("editovanje-teme-na-forumu")).isEqualTo(true);
+    }
+
+    @Test
+    public void testAsistentEditovanjeTemeForum() throws Exception {
+        Uloga asistent = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT);
+        assertThat(asistent.imaPrivilegiju("editovanje-teme-na-forumu")).isEqualTo(true);
+    }
+
+    @Test
+    public void testStudentEditovanjeTemeForum() throws Exception {
+        Uloga student = ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENT);
+        assertThat(student.imaPrivilegiju("editovanje-teme-na-forumu")).isEqualTo(false);
+    }
+
+    @Test
+    public void testPostojiEditovanjeKorisnika() throws Exception {
+        assertThat(privilegijaRepozitorij.existsBynazivPrivilegije("editovanje-korisnika")).isEqualTo(true);
+    }
+
+    @Test
+    public void testAdminEditovanjeKorisnika() throws Exception {
+        Uloga admin = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ADMIN);
+        assertThat(admin.imaPrivilegiju("editovanje-korisnika")).isEqualTo(true);
+    }
+
+    @Test
+    public void testProfesorEditovanjeKorisnika() throws Exception {
+        Uloga profesor = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
+        assertThat(profesor.imaPrivilegiju("editovanje-korisnika")).isEqualTo(false);
+    }
+
+    @Test
+    public void testPostojiBrisanjeObavjestenja() throws  Exception {
+        assertThat(privilegijaRepozitorij.existsBynazivPrivilegije("brisanje-obavjestenja")).isEqualTo(true);
+    }
+
+    @Test
+    public void testAdminBrisanjeObavjestenja() throws Exception {
+        Uloga admin = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ADMIN);
+        assertThat(admin.imaPrivilegiju("brisanje-obavjestenja")).isEqualTo(true);
+    }
+
+    @Test
+    public void testStudentBrisanjeObavjestenja() throws Exception {
+        Uloga student = ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENT);
+        assertThat(student.imaPrivilegiju("brisanje-obavjestenja")).isEqualTo(false);
+    }
+
+    @Test
+    public void testPostojiPregledPredmetaSaradnik() throws  Exception {
+        assertThat(privilegijaRepozitorij.existsBynazivPrivilegije("pregled-predmeta-saradnik")).isEqualTo(true);
+    }
+
+    @Test
+    public void testProfesorPregledPredmeta() throws Exception {
+        Uloga profesor = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
+        assertThat(profesor.imaPrivilegiju("pregled-predmeta-saradnik")).isEqualTo(true);
+    }
+
+    @Test
+    public void testStudentPregledKaoSaradnik() throws Exception {
+        Uloga student = ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENT);
+        assertThat(student.imaPrivilegiju("pregled-predmeta-saradnik")).isEqualTo(false);
+    }
+
+    @Test
+    public void testPostojiPregledGrupaSaradnik() throws  Exception {
+        assertThat(privilegijaRepozitorij.existsBynazivPrivilegije("pristup-grupama-saradnik")).isEqualTo(true);
+    }
+
+    @Test
+    public void testAsistentPregledGrupa() throws Exception {
+        Uloga asistent = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT);
+        assertThat(asistent.imaPrivilegiju("pristup-grupama-saradnik")).isEqualTo(true);
+    }
+
+    @Test
+    public void testStudentPregledGrupaKaoSaradnik() throws Exception {
+        Uloga student = ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENT);
+        assertThat(student.imaPrivilegiju("pristup-grupama-saradnik")).isEqualTo(false);
+    }
+
+    @Test
+    public void testPostojiUvidUKomentareSaradnik() throws  Exception {
+        assertThat(privilegijaRepozitorij.existsBynazivPrivilegije("uvid-u-komentare-saradnik")).isEqualTo(true);
+    }
+
+    @Test
+    public void testAsistentUvidUKomentare() throws Exception {
+        Uloga asistent = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT);
+        assertThat(asistent.imaPrivilegiju("uvid-u-komentare-saradnik")).isEqualTo(true);
+    }
+
+    @Test
+    public void testStudentUvidUKomentareKaoSaradnik() throws Exception {
+        Uloga student = ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENT);
+        assertThat(student.imaPrivilegiju("uvid-u-komentare-saradnik")).isEqualTo(false);
+    }
+
+    @Test
+    public void testPostojiIzborNacinaSlanja() throws  Exception {
+        assertThat(privilegijaRepozitorij.existsBynazivPrivilegije("izbor-nacina-slanja-zadace")).isEqualTo(true);
+    }
+
+    @Test
+    public void testProfesorBiraNacinSlanja() throws Exception {
+        Uloga profesor = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
+        assertThat(profesor.imaPrivilegiju("izbor-nacina-slanja-zadace")).isEqualTo(true);
+    }
+
+    @Test
+    public void testStudentBiraNacinSlanja() throws Exception {
+        Uloga student = ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENT);
+        assertThat(student.imaPrivilegiju("izbor-nacina-slanja-zadace")).isEqualTo(false);
+    }
+
+    @Test
+    public void testPostojiUnosKonacneOcjene() throws  Exception {
+        assertThat(privilegijaRepozitorij.existsBynazivPrivilegije("unos-konacne-ocjene")).isEqualTo(true);
+    }
+
+    @Test
+    public void testProfesorUnosiKonacnuOcjenu() throws Exception {
+        Uloga profesor = ulogaRepozitorij.findBynazivUloge(ImenaUloga.PROFESOR);
+        assertThat(profesor.imaPrivilegiju("unos-konacne-ocjene")).isEqualTo(true);
+    }
+
+    @Test
+    public void testAsistentUnosiKonacnuOcjenu() throws Exception {
+        Uloga asistent = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT);
+        assertThat(asistent.imaPrivilegiju("unos-konacne-ocjene")).isEqualTo(false);
+    }
+}

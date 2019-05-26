52c3dde4b4386666177e26af26fea3529dd6996c
mahirabuturovic <mbuturovic1@etf.unsa.ba>
2019-05-26 20:02:42 +0200

    nova privilegija i testovi

---
+++ demo/src/test/java/com/example/demo/PrivilegijaTest.java
@@ -0,0 +1,56 @@
+package com.example.demo;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
+public class PrivilegijaTest {
+    @Autowired
+    private PrivilegijaRepozitorij privilegijaRepozitorij;
+    @Autowired
+    private KorisnikRepozitorij korisnikRepozitorij;
+
+    @Autowired
+    private OdsjekRepozitorij odsjekRepozitorij;
+
+    @Autowired
+    private UlogaRepozitorij ulogaRepozitorij;
+
+    @Test
+    public void testPostojiPrivilegijaZaPregledDostupnihTemaZaZavrsni(){
+        boolean nadji_privilegiju=privilegijaRepozitorij.existsBynazivPrivilegije("pregled-dostupnih-tema-za-zavrsni-rad");
+        assertEquals(true, nadji_privilegiju);
+    }
+    @Test
+    public void testStudentImaPrivilegijuPregledaDostupnihTemaZaZavrsni(){
+        Uloga uloga=ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENT);
+        assertEquals(true, (uloga.imaPrivilegiju("pregled-dostupnih-tema-za-zavrsni-rad")));
+    }
+    @Test
+    public void testPostojiPrivilegijaZaGenerisanjePoruka(){
+        boolean nadji_privilegiju=privilegijaRepozitorij.existsBynazivPrivilegije("slanje-generisanih-poruka");
+        assertEquals(true, nadji_privilegiju);
+    }
+    @Test
+    public void testStudentskaSluzbaImaPrivilegijuGenerisanjaPoruka(){
+        Uloga uloga=ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENTSKA_SLUZBA);
+        assertEquals(true, (uloga.imaPrivilegiju("slanje-generisanih-poruka")));
+    }
+    @Test
+    public void testPostojiPrivilegijaZaUvidUBodoveNaZadace(){
+        boolean nadji_privilegiju=privilegijaRepozitorij.existsBynazivPrivilegije("uvid-u-azurirane-bodove-na-zadace");
+        assertEquals(true, nadji_privilegiju);
+    }
+    @Test
+    public void testStudentImaPrivilegijuZaUvidUBodoveNaZadace(){
+        Uloga uloga=ulogaRepozitorij.findBynazivUloge(ImenaUloga.STUDENT);
+        assertEquals(true, (uloga.imaPrivilegiju("uvid-u-azurirane-bodove-na-zadace")));
+    }
+
+}

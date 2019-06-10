36be5d9a0cecd704b7c065e525981a7f6d57d1b1
Zerina <zerina.ramic@live.com>
2019-06-10 20:01:50 +0200

    VERZIJA ZA NASTAVAK RADA

--- demo/src/main/java/com/example/demo/PrivilegijaKontroler.java
+++ demo/src/main/java/com/example/demo/PrivilegijaKontroler.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @RestController
+@RequestMapping(value = "/privilegije")
 public class PrivilegijaKontroler {
     private PrivilegijaRepozitorij privilegijaRepozitorij;
 

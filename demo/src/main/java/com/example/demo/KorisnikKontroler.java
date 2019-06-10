6ceb0aba8a9dac229b81dc9c5c90146f935e7493
Zerina <zerina.ramic@live.com>
2019-06-10 17:58:41 +0200

    dodan API privilegijeKorisnika

--- demo/src/main/java/com/example/demo/KorisnikKontroler.java
+++ demo/src/main/java/com/example/demo/KorisnikKontroler.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class KorisnikKontroler {
@@ -17,5 +21,19 @@ public class KorisnikKontroler {
         this.korisnikRepozitorij = korisnikRepozitorij;
         this.privilegijaRepozitorij = privilegijaRepozitorij;
         this.ulogaRepozitorij = ulogaRepozitorij;
+    }
+
+    @RequestMapping(value = "/pretragaId/{idKorisnika}/dajPrivilegije", method = RequestMethod.GET)
+    public List<String> privilegijeKorisnika (@PathVariable Long idKorisnika) {
+        List<Privilegija> privilegije;
+        List<String> povratna = new ArrayList<String>();
+        if (korisnikRepozitorij.findById(idKorisnika).isPresent()) {
+            privilegije = korisnikRepozitorij.findById(idKorisnika).get().getUloga_id().getPrivilegije();
+            for (Privilegija p : privilegije) {
+                povratna.add(p.getNazivPrivilegije());
+            }
+            return povratna;
+        }
+        return null;
     }
 }

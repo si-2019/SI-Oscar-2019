47a2ca659d79f8d5c50f9267df16f91dc48f318e
Zerina <zerina.ramic@live.com>
2019-06-10 19:20:18 +0200

    dodan test za API privilegijeUloge kada uloga ne postoji u bazi

--- demo/src/test/java/com/example/demo/TestoviPrivilegija.java
+++ demo/src/test/java/com/example/demo/TestoviPrivilegija.java
@@ -890,4 +890,14 @@ public class TestoviPrivilegija {
         String body = IOUtils.toString(in, Charset.forName("UTF-8"));
         assertEquals(false, body.isEmpty());
     }
+
+    @Test
+    public void testPrivilegijeUlogeUlogaNePostoji() throws IOException {
+        URL url = new URL("http://localhost:31915/uloga/10000/privilegije");
+        URLConnection con = url.openConnection();
+        InputStream in = con.getInputStream();
+        String body = IOUtils.toString(in, Charset.forName("UTF-8"));
+        JSONArray ja = new JSONArray();
+        assertEquals(ja.toString(), body);
+    }
 }

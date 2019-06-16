b3bc5ad8ab3033a9d3305026786c465b1b128bd5
Amina Fajić <37187803+aminaanaan@users.noreply.github.com>
2019-06-16 19:31:45 +0200

    Update TestoviPrivilegija.java
    commit test

--- demo/src/test/java/com/example/demo/TestoviPrivilegija.java
+++ demo/src/test/java/com/example/demo/TestoviPrivilegija.java
@@ -1191,7 +1191,11 @@ public class TestoviPrivilegija {
         String body = IOUtils.toString(in, Charset.forName("UTF-8"));
         assertSame("Specificirana uloga ili privilegija ne postoje!", body);
     }
-
+ @Test
+    public void testAsistentEditovanjeKreiranogProjekta() throws Exception {
+        Uloga profesor = ulogaRepozitorij.findBynazivUloge(ImenaUloga.ASISTENT);
+        assertThat(profesor.imaPrivilegiju("editovanje-kreiranog-projekta")).isEqualTo(true);
+    }
 }
 
 
